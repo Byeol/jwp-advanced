96e60915d48f6732267df70479adc5704daa6017
Byeol <Byeol@DalComui-MacBook-Pro.local>
2016-06-21 14:00:00 +0900

    Initial commit

---
+++ src/main/java/org/nhnnext/Application.java
@@ -0,0 +1,13 @@
+package org.nhnnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
