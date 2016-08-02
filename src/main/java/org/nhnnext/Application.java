0e0b72ca11aef48ad276712f1619e8bb010b3e48
Jaewon Seo <byeol@hotmail.co.kr>
2016-08-02 10:00:00 +0900

    Upgrade to Spring Boot 1.4.0.RELEASE

--- src/main/java/org/nhnnext/Application.java
+++ src/main/java/org/nhnnext/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
