1e513e84f0b260533752c652b9134e94ae1b078f
Jaewon Seo <byeol@hotmail.co.kr>
2016-08-02 14:00:00 +0900

    Improve templates

--- src/main/java/org/nhnnext/web/actual/BoardController.java
+++ src/main/java/org/nhnnext/web/actual/BoardController.java
@@ -1,11 +1,14 @@
 package org.nhnnext.web.actual;
 
 import org.nhnnext.domain.Board;
+import org.nhnnext.domain.Card;
+import org.nhnnext.domain.List;
 import org.nhnnext.service.actual.BoardService;
+import org.nhnnext.service.actual.CardService;
+import org.nhnnext.service.actual.ListService;
 import org.nhnnext.web.RepositoryEntityController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/boards")
@@ -14,5 +17,28 @@ public class BoardController extends RepositoryEntityController<Board, Long> {
     @Autowired
     public BoardController(BoardService repository) {
         super(repository);
+    }
+
+    @Autowired
+    private BoardService repository;
+
+    @Autowired
+    private ListService listService;
+
+    @Autowired
+    private CardService cardService;
+
+    @RequestMapping(value = "/{id}/lists", method = RequestMethod.POST)
+    public void createList(@PathVariable Long id, @RequestBody List list) {
+        Board board = repository.findOne(id);
+        list.setBoard(board);
+        listService.save(list);
+    }
+
+    @RequestMapping(value = "/{id}/lists/{listId}/cards", method = RequestMethod.POST)
+    public void createCard(@PathVariable Long id, @PathVariable Long listId, @RequestBody Card card) {
+        List list = listService.findOne(listId);
+        card.setList(list);
+        cardService.save(card);
     }
 }
